306321320d47b91a588cca5115209a5994fca38c
Serhiy Korytskyy <korysergey55@gmail.com>
2021-04-09 13:36:28 +0300

    change-color-body

---
+++ js/js-01.js
@@ -0,0 +1,33 @@
+//тип данных
+const a = 10;
+const b = 5;
+const c = a + b;
+const d = "hello world"
+
+console.log(c);
+console.log(d, c);
+console.log(typeof(c));
+console.log(typeof(d));
+
+const type = typeof(c);
+console.log(type);
+
+const typeInput = typeof(d);
+console.log(typeInput);
+
+//преобразование к числу
+let serj = "30 years";
+serj = Number.parseInt(serj);
+console.log('My age is', serj, 'years old');
+
+// Возведение в степень  
+// let userNumb = prompt('Введите число');
+// userNumb = Number(userNumb);
+// console.log(typeof(userNumb));
+
+// let userPow = prompt('Введите степень');
+// userPow = Number(userPow);
+
+// let result = Math.pow(userNumb, userPow);
+// console.log(result);
+
