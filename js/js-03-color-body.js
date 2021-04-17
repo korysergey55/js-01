f8913e2a72b0cc8c9cfd13850909c66d498ef9ed
Serhiy Korytskyy <korysergey55@gmail.com>
2021-04-17 16:12:46 +0300

    refactoring

--- js/js-03-color-body.js
+++
@@ -1,9 +0,0 @@
-//  Замена цвета в body
-const colors = ['tomato', 'teal', 'orange', 'green', 'skyblue', 'deeppink'];
-const min = 0;
-const max = colors.length -1;
-const randomColor = Math.round(Math.random() * (max - min) + min);
-const color =  colors[randomColor];
-console.log(color);
-document.body.style.backgroundColor = color;
-
